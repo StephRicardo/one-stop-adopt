45af1ef78eb57d96ec050be0ee818f40be289578
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2017-05-27 17:26:48 -0400

    new styles and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		puts @user.session
 	end
+
+	# For registering to the site
+	def new
+    @user = User.new
+  end
+
+  def create
+  	 @user = User.create(user_params)
+  	 redirect_to users_path
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
