027e1386c2c98bf9d7996f132e89e4a15fab504c
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2017-05-29 01:02:16 -0400

    Theme change with Flatly in public

--- app/controllers/pet_controller.rb
+++ app/controllers/pet_controller.rb
@@ -1,8 +1,9 @@
 class PetController < ApplicationController
 	def index
+		# @pets = Pet.paginate(page: params[:page])
 	end
 
 	def show
 	end
-	
+ 
 end
--- app/helpers/pet_helper.rb
+++ app/helpers/pet_helper.rb
@@ -1,2 +1,15 @@
 module PetHelper
+
+	def liked_pets
+	end
+
+	def likes?
+	end
+
+	def like
+	end
+
+	def unlike
+	end
+
 end
